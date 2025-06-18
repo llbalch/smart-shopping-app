9a3da45fed8146aad214fe2d0ba50c6d83832211
Lauren Balch <llbalch@gmail.com>
2025-06-18 14:06:55 -0500

    Initial Commit - setup app via Vite and remove stock data and images

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img />
+        <h2>Smart Shopping List</h2>
+      </header>
+
+      <div>Content</div>
+    </div>
+  );
+}
+
+export default App;
