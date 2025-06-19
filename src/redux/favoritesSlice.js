0a707f2cb82dff161cd58ca495340a0440cc1465
Lauren Balch <llbalch@gmail.com>
2025-06-19 15:59:26 -0500

    Added logic to reducers

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,21 +1,62 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: [],
+  suggestions: [
+    /* add logic to suggest items based on previous purchases*/
+  ],
+};
+
 const favoritesSlice = createSlice({
   name: "favoritesList",
-  initialState: [],
+  initialState,
   reducers: {
-    addFavorite: (state, action) => {
-      /* add item logic*/
+    addFavoriteToShoppingList: (state, action) => {
+      // action.payload = item object
+      const exists = state.items.some(
+        (item) => item.name.toLowerCase() === action.payload.name.toLowerCase()
+      );
+      if (!exists) {
+        state.items.push({
+          ...action.payload,
+          completed: false,
+          quantity: 1 /* will need to modify */,
+          note: "",
+        });
+      }
     },
     removeFavorite: (state, action) => {
-      /* remove item logic*/
+      // action.payload = { id }
+      state.items = state.items.filter((item) => item.id !== action.payload);
     },
     editFavorite: (state, action) => {
       /* edit item logic*/
     },
+    setSuggestions: (state, action) => {
+      //action.payload = array of suggested items
+      state.suggestions = action.payload;
+    },
+    addFavoriteByName: (state, action) => {
+      // action.payload = name
+      const exists = state.items.some(
+        (item) => item.name.toLowerCase() === action.payload.toLowerCase()
+      );
+      if (!exists) {
+        state.items.push({
+          id: Date.now().toString(),
+          name: action.payload,
+          category: "",
+        });
+      }
+    },
   },
 });
 
-export const { addFavorite, removeFavorite, editFavorite } =
-  favoritesSlice.actions;
+export const {
+  addFavoriteToShoppingList,
+  removeFavorite,
+  editFavorite,
+  setSuggestions,
+  addFavoriteByName,
+} = favoritesSlice.actions;
 export default favoritesSlice.reducer;
