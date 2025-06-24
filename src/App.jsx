17aa6a75dbfe71e71b65891c660b55ec8d5e4c8a
Lauren Balch <llbalch@gmail.com>
2025-06-24 08:45:35 -0500

    Updated App with panel navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
-import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import styles from "./App.module.css";
+import ShoppingList from "./components/ShoppingList/ShoppingList";
+import {
+  setActiveView,
+  openEditItemModal,
+  closeEditItemModal,
+  openEditCategoryModal,
+  closeEditCategoryModal,
+} from "./redux/uiSlice";
+import EditItemDetails from "../src/components/EditItemDetails/EditItemDetails";
+import EditCategoryDetails from "../src/components/EditCategoryDetails/EditCategoryDetails";
+import FavoritedItems from "../src/components/FavoritedItems/FavoritedItems";
+import Modal from "../src/components/Modal/Modal";
 
 function App() {
+  const {
+    activeView,
+    isEditItemModalOpen,
+    isEditCategoryModalOpen,
+    editingItemId,
+  } = useSelector((state) => state.ui);
+  const dispatch = useDispatch();
   return (
     <div>
-      <header>
-        <img />
-        <h2>Smart Shopping List</h2>
-      </header>
+      {activeView === "shoppingList" && <ShoppingList />}
 
-      <div>Content</div>
+      {activeView === "favorites" && (
+        <FavoritedItems
+          onBack={() => dispatch(setActiveView("shoppingList"))}
+        />
+      )}
+      {isEditItemModalOpen && (
+        <Modal onClose={() => dispatch(closeEditItemModal())}>
+          <EditItemDetails
+            itemId={editingItemId}
+            onClose={() => dispatch(closeEditItemModal())}
+            onEditCategory={() => dispatch(openEditCategoryModal())}
+          />
+        </Modal>
+      )}
+      {isEditCategoryModalOpen && (
+        <Modal onClose={() => dispatch(closeEditCategoryModal())}>
+          <EditCategoryDetails
+            onSave={() => {
+              dispatch(closeEditCategoryModal());
+              dispatch(openEditItemModal(editingItemId));
+            }}
+            onBack={() => {dispatch(closeEditCategoryModal())
+              dispatch(openEditItemModal(editingItemId))
+            }}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
