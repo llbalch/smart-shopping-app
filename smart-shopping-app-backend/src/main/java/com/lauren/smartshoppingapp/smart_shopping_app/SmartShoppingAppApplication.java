4214d0decf70d5d14b874792f18942530c4013db
Lauren Balch <llbalch@gmail.com>
2025-11-07 16:27:49 -0600

    Backend setup with Spring Boot

---
+++ smart-shopping-app-backend/src/main/java/com/lauren/smartshoppingapp/smart_shopping_app/SmartShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.lauren.smartshoppingapp.smart_shopping_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartShoppingAppApplication.class, args);
+	}
+
+}
