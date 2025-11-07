86b3e5489c291eeb36ab9a46914ba9278835c72a
Lauren Balch <llbalch@gmail.com>
2025-11-07 17:24:28 -0600

    add test configuration and security settings

---
+++ smart-shopping-app-backend/src/test/java/com/lauren/smartshoppingapp/smart_shopping_app/config/TestSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.lauren.smartshoppingapp.smart_shopping_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SmartShoppingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
