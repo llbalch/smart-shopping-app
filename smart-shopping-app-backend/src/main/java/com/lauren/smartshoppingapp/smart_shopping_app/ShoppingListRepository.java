0f82e5e6a1a4cb7fb09cc4ad2be2916b71a1473d
Lauren Balch <llbalch@gmail.com>
2025-12-05 13:47:28 -0600

    Add missing repository interfaces

---
+++ smart-shopping-app-backend/src/main/java/com/lauren/smartshoppingapp/smart_shopping_app/ShoppingListRepository.java
@@ -0,0 +1,8 @@
+package com.lauren.smartshoppingapp.smart_shopping_app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
+}
