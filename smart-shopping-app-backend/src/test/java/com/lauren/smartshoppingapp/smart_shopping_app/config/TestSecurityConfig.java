6f69f205a9fc20684f375154ee3ad65dab185c3d
Lauren Balch <llbalch@gmail.com>
2025-12-12 09:17:10 -0600

    updated duplicate file name

--- smart-shopping-app-backend/src/test/java/com/lauren/smartshoppingapp/smart_shopping_app/config/TestSecurityConfig.java
+++ smart-shopping-app-backend/src/test/java/com/lauren/smartshoppingapp/smart_shopping_app/config/TestSecurityConfig.java
@@ -1,14 +1,19 @@
-package com.lauren.smartshoppingapp.smart_shopping_app;
+package com.lauren.smartshoppingapp.smart_shopping_app.config;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class SmartShoppingAppApplicationTests {
+@TestConfiguration
+public class TestSecurityConfig {
 
-    @Test
-    void contextLoads() {
+    @Bean
+    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
     }
 }
